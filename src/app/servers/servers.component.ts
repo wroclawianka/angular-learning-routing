2b710b618f831cc82233d8737b45a3729a163ecf
wroclawianka <dorota.zelga@gmail.com>
2018-07-07 17:03:57 +0200

    Reload Page - learning relative paths in programmatic navigation

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,18 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload() {
+    // this.router.navigate(["/servers"]); works the same as: this.router.navigate(["servers"]);
+    // because navigate method doesn't know on which router you already are. To inform mathod use property relativeTo: 
+    // this.router.navigate(["servers"], {relativeTo: this.route}); (this method cause an error by purpose)
+    // ActivatedRoute injects activated route - see constructor and imports above
+    console.log("Nothing should happen by purpose");
   }
 
 }
