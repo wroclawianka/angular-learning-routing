1b710d6db3a2fb25ed2213373a5d68d3088e8d02
wroclawianka <dorota.zelga@gmail.com>
2018-07-08 13:06:57 +0200

    Edit Server routing added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,4 +19,8 @@ export class HomeComponent implements OnInit {
     // ['/servers', ...] - elements of your path
   }
 
+  onLoadServer(id: number){
+    this.router.navigate(['/servers', id, 'edit'], {queryParams: {allowEdit: 1}, fragment: 'loading'});
+  }
+
 }
